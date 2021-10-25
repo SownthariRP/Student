c363452b2fb5e8c6100140ae5a1b0bfccae5717f
SownthariRP <sowntharirp.20cse@kongu.edu>
2021-10-25 19:00:50 +0530

    First Commit

---
+++ Details.java
@@ -0,0 +1,23 @@
+public class Details
+{
+	public static void main(String[] args)
+	{
+	    String name = "Sownthari R P",fathername="Ponnusamy",Mothername="Rajeswari";
+	    String email="sowntharirp.20cse@kongu.edu";
+	    String address="2/38,pongupalayam,perumanallur",bloodgrp="o-ve";
+	    String rollno="20CSR205";
+	    int age=18;
+	    String aadharno="6745 7324 1012";
+	    String mobileno="9876543210";
+		System.out.println("Name:"+name);
+		System.out.println("Roll number:"+rollno);
+		System.out.println("Father name:"+fathername);
+		System.out.println("Mother name:"+Mothername);
+		System.out.println("Age:"+age);
+		System.out.println("Mobile Number:"+mobileno);
+		System.out.println("Email:"+email);
+		System.out.println("Blood group:"+bloodgrp);
+		System.out.println("Address:"+address);
+		System.out.println("AAdhar number"+aadharno);
+	}
+}
