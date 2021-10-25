3bfe642f602b1013967ac5bec87d1646abeefa63
SownthariRP <sowntharirp.20cse@kongu.edu>
2021-10-25 19:08:29 +0530

    Insurance Commit

---
+++ Insdetail.java
@@ -0,0 +1,20 @@
+public class Insdetail
+{
+	public static void main(String[] args)
+	{
+	    String name = "Sownthari R P",nomineename="Ponnusamy",relation="Father";
+	    String occupation="Farmer";
+	    String email="ponnusamy04061967@gmail.com";
+	    int age=54;
+	    String aadharno="6745 7324 1012";
+	    String mobileno="9876543210";
+		System.out.println("Student Name:"+name);
+		System.out.println("Nominee name:"+nomineename);
+		System.out.println("Relationship of Nominee:"+relation);
+		System.out.println("Nominee Age:"+age);
+		System.out.println("Nominee Mobile Number:"+mobileno);
+		System.out.println("Nominee occupation:"+occupation);
+		System.out.println("Nominee Email:"+email);
+		System.out.println("Nominee Aadhar number"+aadharno);
+	}
+}
