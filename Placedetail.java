cdbafe2fd91d282891d5d095af5bcb9976d21ca6
SownthariRP <sowntharirp.20cse@kongu.edu>
2021-10-25 19:17:51 +0530

    Placement Commit

---
+++ Placedetail.java
@@ -0,0 +1,21 @@
+public class Placedetail
+{
+	public static void main(String[] args)
+	{
+	    String name = "Sownthari R P";
+	    String konemail="sowntharirp.20cse@kongu.edu";
+	    String permail="sownthari01052003@gmail.com";
+	    String rollno="20CSR205";
+	    int arrears=0;
+	    int ds_h=0;
+	    String mobileno="9876543210";
+		System.out.println("Name:"+name);
+		System.out.println("Roll number:"+rollno);
+		System.out.println("Mobile Number:"+mobileno);
+		System.out.println("Edu Email:"+konemail);
+		System.out.println("Personal Email:"+permail);
+		System.out.println("No of arrears:"+arrears);
+		System.out.println("Dayscholar/Hosteller"+ds_h);
+
+	}
+}
