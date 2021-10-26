df25ef7a45c15c0dc60b8321a626f1aeae7d293f
SownthariRP <sowntharirp.20cse@kongu.edu>
2021-10-26 18:59:46 +0530

    course complete

---
+++ coursedetail.java
@@ -0,0 +1,19 @@
+public class coursedetail
+{
+	public static void main(String[] args)
+	{
+	    String rollno="20CSR205";
+	    int semester=3;
+	    int no1cer=2;
+	    String one="aaa",two="bbb";
+	    int noval=0;
+	    String val1="nil";
+	    System.out.println("Roll number:"+rollno);
+	    System.out.println("Semester"+semester);
+	    System.out.println("Number of 1 credit courses completed:"+no1cer);
+	    System.out.println("Name of 1 credit courses completed:"+one+ " "+two);
+	    System.out.println("Number of value added courses completed:"+noval);
+	    System.out.println("Name of value added courses completed:"+val1);
+	    
+	}
+}
